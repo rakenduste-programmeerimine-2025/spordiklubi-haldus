eb50045d7b393a7a572cd649cc6dbf1d7ae433aa
ArminJaemaa <ajaemaa@tlu.ee>
2025-11-27 14:02:28 +0200

    fix(migrations): fix database migration file and replace int with uuid

--- supabase/migrations/20251113114537_spordihaldus-create.sql
+++ supabase/migrations/20251113114537_spordihaldus-create.sql
@@ -3,26 +3,26 @@
 
 -- tables
 -- Table: Club
-CREATE TABLE Club (
+CREATE TABLE club (
     id int  NOT NULL,
     name varchar(50)  NOT NULL,
     created_at timestamp  NOT NULL,
     club_logo varchar(50)  NOT NULL,
     invite_code varchar(100)  NOT NULL,
-    Club_invite_id int  NOT NULL,
-    CONSTRAINT Club_pk PRIMARY KEY (id)
+    club_invite_id int  NOT NULL,
+    CONSTRAINT club_pk PRIMARY KEY (id)
 );
 
 -- Table: Club_invite
-CREATE TABLE Club_invite (
+CREATE TABLE club_invite (
     id int  NOT NULL,
     token varchar(100)  NOT NULL,
     created_at timestamp  NOT NULL,
-    CONSTRAINT Club_invite_pk PRIMARY KEY (id)
+    CONSTRAINT club_invite_pk PRIMARY KEY (id)
 );
 
 -- Table: Event
-CREATE TABLE Event (
+CREATE TABLE event (
     id int  NOT NULL,
     title varchar(100)  NOT NULL,
     description varchar(200)  NOT NULL,
@@ -31,65 +31,64 @@ CREATE TABLE Event (
     end_time time  NULL,
     location varchar(50)  NOT NULL,
     event_type_id int  NOT NULL,
-    Club_id int  NOT NULL,
-    CONSTRAINT Event_pk PRIMARY KEY (id)
+    club_id int  NOT NULL,
+    CONSTRAINT event_pk PRIMARY KEY (id)
 );
 
 -- Table: Event_rsvp
-CREATE TABLE Event_rsvp (
+CREATE TABLE event_rsvp (
     id int  NOT NULL,
-    User_id int  NOT NULL,
-    Event_id int  NOT NULL,
+    profile_id uuid  NOT NULL,
+    event_id int  NOT NULL,
     status boolean  NULL,
     reason varchar(100)  NULL,
-    CONSTRAINT Event_rsvp_pk PRIMARY KEY (id)
+    CONSTRAINT event_rsvp_pk PRIMARY KEY (id)
 );
 
 -- Table: Forum_comment
-CREATE TABLE Forum_comment (
+CREATE TABLE forum_comment (
     id int  NOT NULL,
-    User_id int  NOT NULL,
+    profile_id uuid  NOT NULL,
     content varchar(150)  NOT NULL,
     created_at timestamp  NOT NULL,
-    Forum_post_id int  NOT NULL,
-    CONSTRAINT Forum_comment_pk PRIMARY KEY (id)
+    forum_post_id int  NOT NULL,
+    CONSTRAINT forum_comment_pk PRIMARY KEY (id)
 );
 
 -- Table: Forum_post
-CREATE TABLE Forum_post (
+CREATE TABLE forum_post (
     id int  NOT NULL,
-    User_id int  NOT NULL,
-    Club_id int  NOT NULL,
+    profile_id uuid  NOT NULL,
+    club_id int  NOT NULL,
     title varchar(50)  NOT NULL,
     content varchar(200)  NOT NULL,
     created_at timestamp  NOT NULL,
-    CONSTRAINT Forum_post_pk PRIMARY KEY (id)
+    CONSTRAINT forum_post_pk PRIMARY KEY (id)
 );
 
 -- Table: Member
-CREATE TABLE Member (
+CREATE TABLE member (
     id int  NOT NULL,
-    User_id int  NOT NULL,
-    Club_id int  NOT NULL,
-    Role_id int  NOT NULL,
-    CONSTRAINT Member_pk PRIMARY KEY (id)
+    profile_id uuid  NOT NULL,
+    club_id int  NOT NULL,
+    role_id int NOT NULL,
+    CONSTRAINT member_pk PRIMARY KEY (id)
 );
 
 -- Table: Role
-CREATE TABLE Role (
+CREATE TABLE role (
     id int  NOT NULL,
     name varchar(10)  NOT NULL,
-    CONSTRAINT Role_pk PRIMARY KEY (id)
+    CONSTRAINT role_pk PRIMARY KEY (id)
 );
 
 -- Table: User
-CREATE TABLE "User" (
-    id int  NOT NULL,
+CREATE TABLE profile (
+    id uuid references auth.users(id) on delete cascade,
     name varchar(50)  NOT NULL,
     email varchar(50)  NOT NULL,
-    password varchar(50)  NOT NULL,
-    Role_id int  NOT NULL,
-    CONSTRAINT User_pk PRIMARY KEY (id)
+    role_id int  NOT NULL,
+    CONSTRAINT user_pk PRIMARY KEY (id)
 );
 
 -- Table: event_type
@@ -101,23 +100,23 @@ CREATE TABLE event_type (
 
 -- foreign keys
 -- Reference: Club_Club_invite (table: Club)
-ALTER TABLE Club ADD CONSTRAINT Club_Club_invite
-    FOREIGN KEY (Club_invite_id)
-    REFERENCES Club_invite (id)  
+ALTER TABLE club ADD CONSTRAINT club_club_invite
+    FOREIGN KEY (club_invite_id)
+    REFERENCES club_invite (id)  
     NOT DEFERRABLE 
     INITIALLY IMMEDIATE
 ;
 
 -- Reference: Event_Club (table: Event)
-ALTER TABLE Event ADD CONSTRAINT Event_Club
-    FOREIGN KEY (Club_id)
-    REFERENCES Club (id)  
+ALTER TABLE Event ADD CONSTRAINT event_club
+    FOREIGN KEY (club_id)
+    REFERENCES club (id)  
     NOT DEFERRABLE 
     INITIALLY IMMEDIATE
 ;
 
 -- Reference: Event_event_type (table: Event)
-ALTER TABLE Event ADD CONSTRAINT Event_event_type
+ALTER TABLE Event ADD CONSTRAINT event_event_type
     FOREIGN KEY (event_type_id)
     REFERENCES event_type (id)  
     NOT DEFERRABLE 
@@ -125,81 +124,81 @@ ALTER TABLE Event ADD CONSTRAINT Event_event_type
 ;
 
 -- Reference: Event_rsvp_Event (table: Event_rsvp)
-ALTER TABLE Event_rsvp ADD CONSTRAINT Event_rsvp_Event
-    FOREIGN KEY (Event_id)
-    REFERENCES Event (id)  
+ALTER TABLE event_rsvp ADD CONSTRAINT event_rsvp_event
+    FOREIGN KEY (event_id)
+    REFERENCES event (id)  
     NOT DEFERRABLE 
     INITIALLY IMMEDIATE
 ;
 
 -- Reference: Event_rsvp_User (table: Event_rsvp)
-ALTER TABLE Event_rsvp ADD CONSTRAINT Event_rsvp_User
-    FOREIGN KEY (User_id)
-    REFERENCES "User" (id)  
+ALTER TABLE event_rsvp ADD CONSTRAINT event_rsvp_profile
+    FOREIGN KEY (profile_id)
+    REFERENCES profile (id)  
     NOT DEFERRABLE 
     INITIALLY IMMEDIATE
 ;
 
 -- Reference: Forum_comment_Forum_post (table: Forum_comment)
-ALTER TABLE Forum_comment ADD CONSTRAINT Forum_comment_Forum_post
-    FOREIGN KEY (Forum_post_id)
-    REFERENCES Forum_post (id)  
+ALTER TABLE forum_comment ADD CONSTRAINT forum_comment_forum_post
+    FOREIGN KEY (forum_post_id)
+    REFERENCES forum_post (id)  
     NOT DEFERRABLE 
     INITIALLY IMMEDIATE
 ;
 
 -- Reference: Forum_comment_User (table: Forum_comment)
-ALTER TABLE Forum_comment ADD CONSTRAINT Forum_comment_User
-    FOREIGN KEY (User_id)
-    REFERENCES "User" (id)  
+ALTER TABLE forum_comment ADD CONSTRAINT forum_comment_profile
+    FOREIGN KEY (profile_id)
+    REFERENCES profile (id)  
     NOT DEFERRABLE 
     INITIALLY IMMEDIATE
 ;
 
 -- Reference: Forum_post_Club (table: Forum_post)
-ALTER TABLE Forum_post ADD CONSTRAINT Forum_post_Club
-    FOREIGN KEY (Club_id)
-    REFERENCES Club (id)  
+ALTER TABLE forum_post ADD CONSTRAINT forum_post_club
+    FOREIGN KEY (club_id)
+    REFERENCES club (id)  
     NOT DEFERRABLE 
     INITIALLY IMMEDIATE
 ;
 
 -- Reference: Forum_post_User (table: Forum_post)
-ALTER TABLE Forum_post ADD CONSTRAINT Forum_post_User
-    FOREIGN KEY (User_id)
-    REFERENCES "User" (id)  
+ALTER TABLE forum_post ADD CONSTRAINT forum_post_profile
+    FOREIGN KEY (profile_id)
+    REFERENCES profile (id)  
     NOT DEFERRABLE 
     INITIALLY IMMEDIATE
 ;
 
 -- Reference: Member_Club (table: Member)
-ALTER TABLE Member ADD CONSTRAINT Member_Club
-    FOREIGN KEY (Club_id)
-    REFERENCES Club (id)  
+ALTER TABLE member ADD CONSTRAINT member_club
+    FOREIGN KEY (club_id)
+    REFERENCES club (id)  
     NOT DEFERRABLE 
     INITIALLY IMMEDIATE
 ;
 
 -- Reference: Member_Role (table: Member)
-ALTER TABLE Member ADD CONSTRAINT Member_Role
-    FOREIGN KEY (Role_id)
-    REFERENCES Role (id)  
+ALTER TABLE member ADD CONSTRAINT member_role
+    FOREIGN KEY (role_id)
+    REFERENCES role (id)  
     NOT DEFERRABLE 
     INITIALLY IMMEDIATE
 ;
 
 -- Reference: Member_User (table: Member)
-ALTER TABLE Member ADD CONSTRAINT Member_User
-    FOREIGN KEY (User_id)
-    REFERENCES "User" (id)  
+ALTER TABLE member ADD CONSTRAINT member_profile
+    FOREIGN KEY (profile_id)
+    REFERENCES profile (id)  
     NOT DEFERRABLE 
     INITIALLY IMMEDIATE
 ;
 
 -- Reference: User_Role (table: User)
-ALTER TABLE "User" ADD CONSTRAINT User_Role
-    FOREIGN KEY (Role_id)
-    REFERENCES Role (id)  
+ALTER TABLE profile ADD CONSTRAINT profile_role
+    FOREIGN KEY (role_id)
+    REFERENCES role (id)  
     NOT DEFERRABLE 
     INITIALLY IMMEDIATE
 ;
