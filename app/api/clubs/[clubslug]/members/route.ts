d7c5f570c37d3d46bc46ace60b1591d309250fcf
ArminJaemaa <ajaemaa@tlu.ee>
2025-12-06 22:50:52 +0200

    feat(settings): add logo and name change functionality to club settings and show correct member stats

---
+++ app/api/clubs/[clubslug]/members/route.ts
@@ -0,0 +1,38 @@
+import { NextResponse } from "next/server"
+import { createClient } from "@/lib/supabase/server"
+
+export async function GET(req: Request, { params }: { params: { clubslug: string } }) {
+      const resolvedParams = await params
+      const {clubslug} = resolvedParams
+  const supabase = await createClient()
+
+  console.log("Received slug:", clubslug)
+
+    const { data: clubData, error: clubError } = await supabase
+  .from("club")
+  .select("id")
+  .eq("slug", clubslug)
+  .single()
+
+if (clubError || !clubData) throw new Error("Club not found")
+
+const clubId = clubData.id
+
+const { data: members, error } = await supabase
+  .from("member")
+  .select(`
+    id,
+    profile:profile_id (
+      id,
+      name,
+      role:role_id (id, name)
+    )
+  `)
+  .eq("club_id", clubId)
+
+  if (error) {
+    return NextResponse.json({ error: error.message }, { status: 400 })
+  }
+
+  return NextResponse.json({ members })
+}
